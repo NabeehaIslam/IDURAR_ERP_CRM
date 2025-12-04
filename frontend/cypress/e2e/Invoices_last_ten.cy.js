a717b749f77c6e3cdb4be47cc6a84ca4f3252346
Maryam1234678 <i233046@isb.nu.edu.pk>
2025-12-04 17:17:00 +0500

    Add new invoice test cases TC20 to TC30
    Added new test cases for invoice creation in Cypress.

--- frontend/cypress/e2e/Invoices_last_ten.cy.js
+++ frontend/cypress/e2e/Invoices_last_ten.cy.js
@@ -1,3 +1,102 @@
+describe("TC29_invoices", () => {
+  it("tests TC29_invoices", () => {
+    cy.viewport(935, 782);
+    cy.visit("http://localhost:3000/login");
+
+    cy.get("#normal_login_email").clear().type("admin@admin.com");
+    cy.get("#normal_login_password").clear().type("admin123");
+    cy.get("#normal_login_remember").click();
+    cy.get('button[type="submit"]').click();
+    cy.contains("Dashboard", { timeout: 8000 }).should("be.visible");
+    cy.contains("Invoices").click();
+    cy.url().should("include", "/invoice");
+    cy.contains("Invoice List", { timeout: 8000 }).should("be.visible");
+    cy.contains("Add New Invoice").click();
+    cy.contains("Client", { timeout: 10000 }).should("be.visible");
+    cy.get(".ant-select-selector").first().click();
+    cy.get(".ant-select-dropdown", { timeout: 5000 }).should("be.visible");
+    cy.get(".ant-select-item-option").first().click();
+    cy.get("#number").clear().type("5") ;
+    cy.get("#year").clear().type("2025");
+    cy.get("#date").clear().type("02/12/2025");
+    cy.get("#expiredDate").clear().type("01/01/2026");
+    cy.get("#notes").clear().type("Good");
+     cy.get("#items_0_itemName").type("Accounting Service");
+    cy.get("#items_0_description").type("101 charLorem ipsum Morbi erat ex, lacinia nec efficitur eget, sagittis ut orci. Etiam in dolor placerat, pharetra ligula et, bibendum neque. Vestibulum vitae congue lectus, sed ultricies augue. Nam iaculis elit nec velit luctus, vitae rutrum nunc imperdiet. Nunc vel turpis sit amet lectus pellentesque tincidunt. Proin commodo tincidunt enim, at sodales mi dictum ac. Maecenas molestie, metus quis malesuada dictum, leo erat egestas lacus, sit amet tristique urna magna a diam. Donec ultricies dui sit amet mi ornare egestas. Phasellus ultricies lectus non interdum pellentesque. Cras nisi tellus, feugiat sed enim quis, tristique interdum lacus. Sed vel pharetra arcu, ac fermentum neque. Morbi mollis sollicitudin varius. Ut sit amet vulputate velit.101 charLorem ipsum Morbi erat ex, lacinia nec efficitur eget, sagittis ut orci. Etiam in dolor placerat, pharetra ligula et, bibendum neque. Vestibulum vitae congue lectus, sed ultricies augue. Nam iaculis elit nec velit luctus, vitae rutrum nunc imperdiet. Nunc vel turpis sit amet lectus pellentesque tincidunt. Proin commodo tincidunt enim, at sodales mi dictum ac. Maecenas molestie, metus quis malesuada dictum, leo erat egestas lacus, sit amet tristique urna magna a diam. Donec ultricies dui sit amet mi ornare egestas. Phasellus ultricies lectus non interdum pellentesque. Cras nisi tellus, feugiat sed enim quis, tristique interdum lacus. Sed vel pharetra arcu, ac fermentum neque. Morbi mollis sollicitudin varius. Ut sit amet vulputate velit.");
+    cy.get("#items_0_quantity").type("1");
+    cy.get("#items_0_price").type("100");
+    cy.contains("Select Tax Value").click({ force: true });
+    
+    cy.get("span").contains("Tax 0%", { matchCase: false }).click();
+    cy.contains("button", "Save").click();
+  });
+});
+describe("TC28_invoices", () => {
+  it("tests TC28_invoices", () => {
+    cy.viewport(935, 782);
+    cy.visit("http://localhost:3000/login");
+
+    cy.get("#normal_login_email").clear().type("admin@admin.com");
+    cy.get("#normal_login_password").clear().type("admin123");
+    cy.get("#normal_login_remember").click();
+    cy.get('button[type="submit"]').click();
+    cy.contains("Dashboard", { timeout: 8000 }).should("be.visible");
+    cy.contains("Invoices").click();
+    cy.url().should("include", "/invoice");
+    cy.contains("Invoice List", { timeout: 8000 }).should("be.visible");
+    cy.contains("Add New Invoice").click();
+    cy.contains("Client", { timeout: 10000 }).should("be.visible");
+    cy.get(".ant-select-selector").first().click();
+    cy.get(".ant-select-dropdown", { timeout: 5000 }).should("be.visible");
+    cy.get(".ant-select-item-option").first().click();
+    cy.get("#number").clear().type("5") ;
+    cy.get("#year").clear().type("2025");
+    cy.get("#date").clear().type("02/12/2025");
+    cy.get("#expiredDate").clear().type("01/01/2026");
+    cy.get("#notes").clear().type("Good");
+     cy.get("#items_0_itemName").type("101 charLorem ipsum Morbi erat ex, lacinia nec efficitur eget, sagittis ut orci. Etiam in dolor placerat, pharetra ligula et, bibendum neque. Vestibulum vitae congue lectus, sed ultricies augue. Nam iaculis elit nec velit luctus, vitae rutrum nunc imperdiet. Nunc vel turpis sit amet lectus pellentesque tincidunt. Proin commodo tincidunt enim, at sodales mi dictum ac. Maecenas molestie, metus quis malesuada dictum, leo erat egestas lacus, sit amet tristique urna magna a diam. Donec ultricies dui sit amet mi ornare egestas. Phasellus ultricies lectus non interdum pellentesque. Cras nisi tellus, feugiat sed enim quis, tristique interdum lacus. Sed vel pharetra arcu, ac fermentum neque. Morbi mollis sollicitudin varius. Ut sit amet vulputate velit.");
+    cy.get("#items_0_description").type("Monthly Retainer");
+    cy.get("#items_0_quantity").type("1");
+    cy.get("#items_0_price").type("100");
+    cy.contains("Select Tax Value").click({ force: true });
+    
+    cy.get("span").contains("Tax 0%", { matchCase: false }).click();
+    cy.contains("button", "Save").click();
+  });
+});
+describe("TC27_invoices", () => {
+  it("tests TC27_invoices", () => {
+    cy.viewport(935, 782);
+    cy.visit("http://localhost:3000/login");
+
+    cy.get("#normal_login_email").clear().type("admin@admin.com");
+    cy.get("#normal_login_password").clear().type("admin123");
+    cy.get("#normal_login_remember").click();
+    cy.get('button[type="submit"]').click();
+    cy.contains("Dashboard", { timeout: 8000 }).should("be.visible");
+    cy.contains("Invoices").click();
+    cy.url().should("include", "/invoice");
+    cy.contains("Invoice List", { timeout: 8000 }).should("be.visible");
+    cy.contains("Add New Invoice").click();
+    cy.contains("Client", { timeout: 10000 }).should("be.visible");
+    cy.get(".ant-select-selector").first().click();
+    cy.get(".ant-select-dropdown", { timeout: 5000 }).should("be.visible");
+    cy.get(".ant-select-item-option").first().click();
+    cy.get("#number").clear().type("5") ;
+    cy.get("#year").clear().type("2025");
+    cy.get("#date").clear().type("02/12/2025");
+    cy.get("#expiredDate").clear().type("01/01/2026");
+    cy.get("#notes").clear().type("Good");
+     cy.get("#items_0_itemName").type("Lorem ipsum Morbi erat ex, lacinia nec efficitur eget, sagittis ut orci. Etiam in dolor placerat, pharetra ligula et, bibendum neque. Vestibulum vitae congue lectus, sed ultricies augue. Nam iaculis elit nec velit luctus, vitae rutrum nunc imperdiet. Nunc vel turpis sit amet lectus pellentesque tincidunt. Proin commodo tincidunt enim, at sodales mi dictum ac. Maecenas molestie, metus quis malesuada dictum, leo erat egestas lacus, sit amet tristique urna magna a diam. Donec ultricies dui sit amet mi ornare egestas. Phasellus ultricies lectus non interdum pellentesque. Cras nisi tellus, feugiat sed enim quis, tristique interdum lacus. Sed vel pharetra arcu, ac fermentum neque. Morbi mollis sollicitudin varius. Ut sit amet vulputate velit.");
+    cy.get("#items_0_description").type("Monthly Retainer");
+    cy.get("#items_0_quantity").type("1");
+    cy.get("#items_0_price").type("100");
+    cy.contains("Select Tax Value").click({ force: true });
+    
+    cy.get("span").contains("Tax 0%", { matchCase: false }).click();
+    cy.contains("button", "Save").click();
+  });
+});
 describe("TC30_invoices", () => {
   it("tests TC30_invoices", () => {
     cy.viewport(935, 782);
@@ -19,7 +118,7 @@ describe("TC30_invoices", () => {
     cy.get("#number").clear().type("5") ;
     cy.get("#year").clear().type("2025");
     cy.get("#date").clear().type("02/12/2025");
-    cy.get("#expiredDate").clear().type("01/01/2025");
+    cy.get("#expiredDate").clear().type("01/01/2026");
     cy.get("#notes").clear().type("Good");
      cy.get("#items_0_itemName").type("Accounting Service");
     cy.get("#items_0_description").type("Monthly Retainer");
@@ -52,7 +151,7 @@ describe("TC22_invoices", () => {
     cy.get("#number").clear().type("5") ;
     cy.get("#year").clear().type("2025");
     cy.get("#date").clear().type("02/12/2025");
-    cy.get("#expiredDate").clear().type("01/01/2025");
+    cy.get("#expiredDate").clear().type("01/01/2026");
     cy.get("#notes").clear().type("Good");
      cy.get("#items_0_itemName").type("Consulting Service");
     cy.get("#items_0_description").type("Monthly Retainer");
@@ -85,7 +184,7 @@ describe("TC21_invoices", () => {
     cy.get("#number").clear().type("5") ;
     cy.get("#year").clear().type("2025");
     cy.get("#date").clear().type("02/12/2025");
-    cy.get("#expiredDate").clear().type("01/01/2025");
+    cy.get("#expiredDate").clear().type("01/01/2026");
     cy.get("#notes").clear().type("Good");
      cy.get("#items_0_itemName").type("Consulting Service");
     cy.get("#items_0_description").type("Monthly Retainer");
@@ -118,7 +217,7 @@ describe("TC20_invoices", () => {
     cy.get("#number").clear().type("5") ;
     cy.get("#year").clear().type("2025");
     cy.get("#date").clear().type("02/12/2025");
-    cy.get("#expiredDate").clear().type("01/01/2025");
+    cy.get("#expiredDate").clear().type("01/01/2026");
     cy.get("#notes").clear().type("Good");
      cy.get("#items_0_itemName").type("Consulting Service");
     cy.get("#items_0_description").type("Monthly Retainer");
